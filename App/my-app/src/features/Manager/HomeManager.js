bf5ac7acc3e3164199467d735066d92493ddbe56
VihaoHT <haoanna1232@gmail.com>
2023-10-19 15:38:07 +0700

    Home Nhan Vien , Home Manager UI

--- App/my-app/src/features/Manager/HomeManager.js
+++ App/my-app/src/features/Manager/HomeManager.js
@@ -1,63 +1,88 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-const home = (props) => {
-  const navigation = useNavigation();
 
-  const goToProblemReportScreen = () => {
-    navigation.navigate("Problem_Report");
-  };
-  const goToSendSupportScreen = () => {
-    navigation.navigate("Send_Support");
-  };
+const HomeManager = (props) => {
+    const navigation = useNavigation();
+
+    const goToNhanSuScreen = () => {
+      navigation.navigate("ListNS");
+    };
   return (
     <View style={styles.container}>
       <View style={styles.userContent}>
         <Image
           style={styles.userAvatar}
-          source={require("../images/avatar.png")}
+          source={require("../../images/avatar.png")}
         />
         <Text style={styles.userName}>Nguyễn Trung Hải</Text>
         <Image
           style={styles.btnNotification}
-          source={require("../images/notifications.png")}
+          source={require("../../images/notifications.png")}
         />
       </View>
       <View style={styles.container2}>
         <Text style={styles.text1}>Dịch vụ trực tuyến</Text>
 
-        <TouchableOpacity
-          style={styles.problemReport}
-          onPress={goToProblemReportScreen}
-        >
+        {/* Nhan Su */}
+        <TouchableOpacity style={styles.problemReport} onPress={goToNhanSuScreen}>
           <Image
             style={styles.imgEdit}
-            source={require("../images/edit.png")}
+            source={require("../../images/nhansu.png")}
           />
-          <Text style={styles.text2}>Báo cáo sự cố</Text>
+          <Text style={styles.text2}>Nhân sự</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.ITSupport} onPress={goToSendSupportScreen}>
-          <Image
-            style={styles.imgEdit}
-            source={require("../images/global.png")}
-          />
-          <Text style={styles.text3}>Yêu cầu hỗ trợ CNTT</Text>
-        </TouchableOpacity>
-
+        {/* Thong Ke Su Co Trong Ngay */}
         <TouchableOpacity style={styles.managerRoom}>
           <Image
             style={styles.imgEdit}
-            source={require("../images/feed.png")}
+            source={require("../../images/thongke.png")}
           />
-          <Text style={styles.text4}>Quản lý mượn phòng học, hội trường</Text>
+          <Text style={styles.text4}>Thống kê sự cố trong ngày</Text>
+        </TouchableOpacity>
+
+        {/* Sự cố chưa hoàn thành */}
+        <TouchableOpacity style={styles.managerRoom}>
+          <Image
+            style={styles.imgEdit}
+            source={require("../../images/notdone.png")}
+          />
+          <Text style={styles.text4}>Sự cố chưa hoàn thành</Text>
+        </TouchableOpacity>
+
+        {/* Thành tích cán bộ */}
+        <TouchableOpacity style={styles.managerRoom}>
+          <Image
+            style={styles.imgEdit}
+            source={require("../../images/thanhtich.png")}
+          />
+          <Text style={styles.text4}>Thành tích cán bộ</Text>
+        </TouchableOpacity>
+
+        {/* Sự kiện bộ môn */}
+        <TouchableOpacity style={styles.managerRoom}>
+          <Image
+            style={styles.imgEdit}
+            source={require("../../images/sukien.png")}
+          />
+          <Text style={styles.text4}>Sự kiện bộ môn</Text>
+        </TouchableOpacity>
+
+        {/* Tính sẵn sàng phòng */}
+        <TouchableOpacity style={styles.managerRoom}>
+          <Image
+            style={styles.imgEdit}
+            source={require("../../images/feature.png")}
+          />
+          <Text style={styles.text4}>Tính sẵn sàng phòng</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 };
 
-export default home;
+export default HomeManager;
 
 const styles = StyleSheet.create({
   container: {
@@ -110,7 +135,7 @@ const styles = StyleSheet.create({
   problemReport: {
     flexDirection: "row",
     width: 343,
-    height: 86,
+    height: 66,
     marginTop: 24,
     alignItems: "center",
     backgroundColor: "#F1F4F5",
@@ -127,31 +152,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "500",
   },
-  ITSupport: {
-    flexDirection: "row",
-    width: 343,
-    height: 86,
-    marginTop: 19,
-    alignItems: "center",
-    backgroundColor: "#F1F4F5",
-    borderRadius: 12,
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.10)",
-    borderWidth: 1,
-    elevation: 2,
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-  },
-  text3: {
-    marginStart: 40,
-    color: "#000",
-    fontSize: 16,
-    fontWeight: "500",
-  },
   managerRoom: {
     flexDirection: "row",
     width: 343,
-    height: 86,
-    marginTop: 19,
+    height: 66,
+    marginTop: 15,
     alignItems: "center",
     backgroundColor: "#F1F4F5",
     borderRadius: 12,
