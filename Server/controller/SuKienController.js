70fb5e9c8a8b1bd5b30cb9ee51075b18cb603c04
Nha0127 <117083457+Nha0127@users.noreply.github.com>
2023-11-14 19:31:11 +0700

    fix

--- Server/controller/SuKienController.js
+++ Server/controller/SuKienController.js
@@ -17,11 +17,10 @@ exports.addSuKien = async (req, res, next) => {
     try {
         const data = await SuKien.create({
             title: req.body.title,
-            thoiGian: req.body.thoiGian,
-            phongBan: req.body.phongBan,
-            moTa: req.body.moTa,
-            diaDiem: req.body.diaDiem,
-            hinhAnh: req.body.hinhAnh ? req.body.hinhAnh : "",
+            thoi_gian: req.body.thoiGian,
+            mo_ta: req.body.moTa,
+            dia_diem: req.body.diaDiem,
+            hinh_anh: req.body.hinhAnh ? req.body.hinhAnh : "",
         })
         res.status(500).json({
             status: 'success',
@@ -35,7 +34,7 @@ exports.addSuKien = async (req, res, next) => {
 }
 exports.deleteSuKien = async (req, res, next) => {
     try {
-        const data = await SuKien.deleteOne({_id: req.params.id})
+        const data = await SuKien.deleteOne({ _id: req.params.id })
         res.status(500).json({
             status: 'success',
             data
