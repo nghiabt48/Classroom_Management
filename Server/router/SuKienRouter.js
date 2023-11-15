3ed53735ac2cc6704aacaf5e54a199690d30a6d0
Nha0127 <117083457+Nha0127@users.noreply.github.com>
2023-11-15 22:23:07 +0700

    Chi tiết sự kiện và thông báo

--- Server/router/SuKienRouter.js
+++ Server/router/SuKienRouter.js
@@ -5,4 +5,5 @@ const SuKienControler = require('../controller/SuKienController')
 router.get('/getSuKien', SuKienControler.getSuKien)
 router.post('/createSuKien', SuKienControler.addSuKien)
 router.delete('/deleteSuKien/:id', SuKienControler.deleteSuKien)
+router.get('/getChiTiet/:id', SuKienControler.getCTSuKien)
 module.exports = router
